60580e21c5f578f395c3a6ef7c4859ffa4a9cafc
Saharfa24 <Sahfad8846@students.ecpi.edu>
2026-02-03 09:59:18 -0500

    Add files via upload

---
+++ VerifyingDBstate.sql
@@ -0,0 +1,16 @@
+/* select USER_ID,
+USERNAME, CREATED,PASSWORD_CHANGE_DATE from the USER_USERS */
+
+select USER_ID,  
+       USERNAME,   
+       CREATED,  
+       PASSWORD_CHANGE_DATE 
+from USER_USERS;
+
+:
+
+
+:
+
+
+:
